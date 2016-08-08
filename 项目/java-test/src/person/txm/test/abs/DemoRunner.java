f0d1a61d625a4d58df59fd7628a5fda2ed32d286
AzadaLee <txm0110@sina.com>
2016-08-08 16:57:03 +0800

    test

--- 项目/java-test/src/person/txm/test/abs/DemoRunner.java
+++
@@ -1,8 +0,0 @@
-package person.txm.test.abs;
-
-public class DemoRunner {
-	public static void main(String[] args) {
-		ImpClass ic = new ImpClass();
-		ic.invoke();
-	}
-}
--- 项目/java-test/src/person/txm/test/abs/ImpClass.java
+++
@@ -1,9 +0,0 @@
-package person.txm.test.abs;
-
-public class ImpClass extends AbsClass{
-
-	@Override
-	protected void exe() {
-		System.out.println("this is impclass exe method !");
-	}
-}
